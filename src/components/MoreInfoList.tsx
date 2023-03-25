744b4197d0c63a0679b61a5021fe20812b693ad2
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-25 14:28:07 -0300

    feat: add more-info-list component

---
+++ src/components/MoreInfoList.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import styled, { css } from "styled-components";
+
+type MoreInfoListProps = {
+	infos: string[];
+	isAccentCheckIcon?: boolean;
+};
+
+export const MoreInfoList = (props: MoreInfoListProps) => {
+	const { infos, isAccentCheckIcon } = props;
+	return (
+		<Container>
+			{infos.map((info, i) => (
+				<MoreInfoListItem key={i}>
+					<Image
+						src={
+							isAccentCheckIcon
+								? "/check-brand-color.svg"
+								: "/check-util-color.svg"
+						}
+						alt="Ícone de Check"
+						width={20}
+						height={20}
+					/>
+					{info}
+				</MoreInfoListItem>
+			))}
+		</Container>
+	);
+};
+
+const Container = styled.ul`
+	${({ theme }) => css`
+		display: flex;
+		flex-direction: column;
+		gap: ${theme.spaces[7]};
+	`}
+`;
+
+const MoreInfoListItem = styled.li`
+	${({ theme }) => css`
+		display: flex;
+		align-items: center;
+		gap: ${theme.spaces[7]};
+		color: ${theme.colors.font.dark};
+		font-size: ${theme.fontSizes.x_small};
+		font-weight: 700;
+		@media screen and (${theme.breakpoints.default}) {
+			font-size: ${theme.fontSizes.small};
+		}
+	`}
+`;
