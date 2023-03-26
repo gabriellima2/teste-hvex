82f5dfda80ff1ca88760b9445434490990cf3cb0
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-26 16:42:16 -0300

    chore: add contacts assets

---
+++ src/assets/contacts.ts
@@ -0,0 +1,6 @@
+import type { IContact } from "@/interfaces/IContact";
+
+export const contacts: IContact[] = [
+	{ href: "#", name: "telefone", value: "(35) 3622-2699" },
+	{ href: "#", name: "email", value: "contato@hvex.com.br" },
+];
