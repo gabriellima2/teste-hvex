c049810c222d8fb3e811075d43a920e1a185c334
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-26 16:28:40 -0300

    chore: add social-networks assets

---
+++ src/assets/social-networks.ts
@@ -0,0 +1,20 @@
+import { ISocialNetwork } from "@/interfaces/ISocialNetwork";
+
+export const socialNetworks: ISocialNetwork[] = [
+	{
+		iconSrc: "/social-networks/linkedin.svg",
+		name: "linkedin",
+	},
+	{
+		iconSrc: "/social-networks/facebook.svg",
+		name: "facebook",
+	},
+	{
+		iconSrc: "/social-networks/instagram.svg",
+		name: "instagram",
+	},
+	{
+		iconSrc: "/social-networks/youtube.svg",
+		name: "youtube",
+	},
+];
