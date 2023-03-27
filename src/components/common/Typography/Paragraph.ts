d9c64d186c30464adee38315b318c547876f280e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-27 10:31:35 -0300

    refactor: improves paragraph variants

--- src/components/common/Typography/Paragraph.ts
+++ src/components/common/Typography/Paragraph.ts
@@ -1,11 +1,21 @@
+import { Modifiers } from "@/@types/Modifiers";
 import styled, { css } from "styled-components";
 
 type ParagraphProps = {
 	secondaryColor?: boolean;
 };
 
+type ParagraphVariants = keyof Pick<ParagraphProps, "secondaryColor">;
+
+const modifiers: Modifiers<ParagraphVariants> = {
+	secondaryColor: (theme) => css`
+		color: ${theme.colors.font.dark};
+	`,
+};
+
 export const Paragraph = styled.p<ParagraphProps>`
-	${({ theme, secondaryColor = false }) => css`
-		color: ${secondaryColor ? theme.colors.font.dark : theme.colors.font.light};
+	${({ theme, secondaryColor }) => css`
+		color: ${theme.colors.font.light};
+		${secondaryColor && modifiers.secondaryColor(theme)};
 	`}
 `;
