97e0fa4f567c8b1809b2c2f26a82f4091f20de5e
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-03-28 15:53:35 -0300

    test: add test for initial values of useWindowSize hook

--- src/hooks/useWindowSize/useWindowSize.test.ts
+++ src/hooks/useWindowSize/useWindowSize.test.ts
@@ -3,21 +3,38 @@ import { act, renderHook } from "@testing-library/react";
 import { useWindowSize } from ".";
 import { defineWindowSize } from "@/__mocks__/define-window-size";
 
+const DEFAULT_SIZES = {
+	WIDTH: 1024,
+	HEIGHT: 768,
+};
 const SIZE = 300;
 
 describe("useWindowSize", () => {
-	beforeAll(() => {
-		defineWindowSize({
-			width: SIZE,
-			height: SIZE,
+	describe("Initial values", () => {
+		it("should return initial values", () => {
+			const { result } = render();
+			expect(result.current.width).toBe(DEFAULT_SIZES.WIDTH);
+			expect(result.current.height).toBe(DEFAULT_SIZES.HEIGHT);
 		});
 	});
-	it("should update sizes state with resize", () => {
-		const { result } = renderHook(useWindowSize);
-		act(() => {
-			window.dispatchEvent(new Event("resize"));
+	describe("Methods", () => {
+		describe("UpdateSizes", () => {
+			beforeAll(() => {
+				defineWindowSize({
+					width: SIZE,
+					height: SIZE,
+				});
+			});
+			it("should update sizes state with resize", () => {
+				const { result } = render();
+				act(() => {
+					window.dispatchEvent(new Event("resize"));
+				});
+				expect(result.current.width).toBe(SIZE);
+				expect(result.current.height).toBe(SIZE);
+			});
 		});
-		expect(result.current.width).toBe(SIZE);
-		expect(result.current.height).toBe(SIZE);
 	});
 });
+
+const render = () => renderHook(useWindowSize);
